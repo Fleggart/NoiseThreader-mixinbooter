ad020a3a302e4a418541fcbdaa7b2d4163b22fca
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-06-03 00:59:28 +0800

    Update NoiseThreader.java

--- src/main/java/noisethreader/NoiseThreader.java
+++ src/main/java/noisethreader/NoiseThreader.java
@@ -9,7 +9,7 @@ import org.apache.logging.log4j.Logger;
 public class NoiseThreader {
     public static final String MODID = "noisethreader";
     public static final String VERSION = "1.1.3";
-    public static final String NAME = "NoiseThreader";
+    public static final String NAME = "NoiseThreader-mixinbooter";
     public static final Logger LOGGER = LogManager.getLogger();
 	
 	@Instance(MODID)
